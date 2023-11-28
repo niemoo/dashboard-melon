52d34226ea84baa96c55c61d063c5b1d13e944ee
Izzan Abdul <izzanabdul123@gmail.com>
2023-11-29 06:31:04 +0700

    feat: fixing

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import { React, useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/melon_biji.png';
 
