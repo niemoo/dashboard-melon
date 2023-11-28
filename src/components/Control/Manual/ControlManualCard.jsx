76683a96983650883027729fe75c878a5656b94b
Izzan Abdul <izzanabdul123@gmail.com>
2023-11-29 06:38:47 +0700

    feat: fixing

--- src/components/Control/Manual/ControlManualCard.jsx
+++ src/components/Control/Manual/ControlManualCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { React, useState } from 'react';
 
 const ControlManualCard = ({ title, disabled }) => {
   const [isChecked, setIsChecked] = useState(false);
